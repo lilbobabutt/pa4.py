fcf0dcae50074e4773683626e8194195084d576d
lilbobabutt <92837734+lilbobabutt@users.noreply.github.com>
2021-10-28 17:28:07 -0700

    Update pa4.py

--- pa4.py
+++ pa4.py
@@ -1,3 +1,14 @@
+#Part 1.1: File Finding
+def extract_hwk_from_paths(paths):
+    p = []
+    for n in range(len(paths)):
+        CSE8A = paths[n].split("/")
+        p.append(CSE8A[-1])
+    return p
+
+
+print(extract_hwk_from_paths(["/home/CSE8A/alex/Hwk1.pdf", "/home/CSE8A/naomi/Hwk1.mp4", "/home/CSE11/jeff/Hwk1.pdf"]))
+
 # 1.2 --> Andre
 def find_and_swap(words, word_one, word_two):
     for i in range(len(words)):
